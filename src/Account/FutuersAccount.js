89076e7ee20473d690f80c8ba7e46484ef1b1f0f
unknown <kiberd22@gmail.com>
2021-07-20 10:52:22 +0900

    21-720

---
+++ src/Account/FutuersAccount.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
+// Node Binance API
+// https://github.com/binance-exchange/node-binance-api
+
+
+
+// BTCUSDT - price
+const FuturesAccount = () => {
+
+  
+
+}
+
+export default FuturesAccount
